b15ac6959e72edb2fe4b7b6c38c0fefc928a1b86
Josse29 <pinemjosse@gmail.com>
2022-09-07 10:14:24 +0700

    fix last

--- assets/js/wishlist.js
+++ assets/js/wishlist.js
@@ -1,11 +1,12 @@
 // initialize 
-		let todos = {};
-		const STORAGE_TODO = "STORAGE_TODO";
-		const todoBox = document.getElementById('todoList');
-		const inputBox = document.querySelector(".inputField input#new_text");
-		const addBtn = document.querySelector(".inputField button");
-		inputBox.onkeyup = ()=>
-		{
+	let todos = {};
+	const STORAGE_WISH = "STORAGE_WISH";
+	const todoBox = document.getElementById('todoList');
+	const inputBox = document.querySelector(".inputField input#new_text");
+	const addBtn = document.querySelector(".inputField button");
+	
+	inputBox.onkeyup = ()=>
+	{
 		let userData = inputBox.value;
 			if(userData.trim()!=0)
 			{
@@ -14,56 +15,68 @@
 			else
 			{
 				// unactive button
-				addBtn.classList.remove('active');
-			}
+			addBtn.classList.remove('active');
 		}
+	}
 
 
-		if( todoFromLocal = localStorage.getItem(STORAGE_TODO))
+	if( todoFromLocal = localStorage.getItem(STORAGE_WISH))
+	{
+		todos = JSON.parse(todoFromLocal);
+		for(let key in todos)
 		{
-			todos = JSON.parse(todoFromLocal);
-			console.log(todos);
-			for(let key in todos)
-			{
-				createList(key, todos[key]);
-			}
+			createList(key, todos[key]);
 		}
-		function syncLocalStorage(activity, item, status = false)
-		{
-			switch(activity)
-			{
-				case 'ADD':		
-				case 'UPDATE':
-					todos[item] = status		
-					break;	
-				default:
-					break;
-			}
-			localStorage.setItem(STORAGE_TODO, JSON.stringify(todos))
-			return
-		}
+	}
 
-		function add()
+	function syncLocalStorage(activity, item, status = false)
+	{
+		switch(activity)
 		{
-			let newText = document.getElementById('new_text');
-			createList(newText.value);
-			syncLocalStorage('ADD', newText.value);
-			alert("Sooner or Later It's gonna be happened");
-			newText.value = '';
+			case 'ADD':		
+			case 'CHECK':
+				todos[item] = status		
+				break;
+			case 'DELETE':
+				delete todos[item]
+				break;	
+			default:
+				break;
 		}
+		localStorage.setItem(STORAGE_WISH, JSON.stringify(todos));
+		return;
+	}
+
+	function add()
+	{
+		let newText = document.getElementById('new_text');
+		createList(newText.value);
+		syncLocalStorage('ADD', newText.value);
+		alert("Sooner or Later It's gonna be happened");
+		newText.value = '';
+	}
 		
-		function createList(text, status=false)
-		{
-			let isDone = (status) ? 'fa-check' : '';
-			let newTodo = `<li > 
-							<span onclick='LOA(this);'>${text}</span>
-							<i class='fa ${isDone}'></i> 
-						  </li>`;
-			todoBox.insertAdjacentHTML('afterbegin',newTodo);
-		}
+	function createList(text, status=false)
+	{
+		let isCheck = (status) ? 'fa-check' : '';
+		let newTodo = 
+		`<li onclick='hapus(this)'> 
+			<span onclick='checked(this)'>${text}</span>
+			<i class='fa ${isCheck}'></i>
+		</li>`;
+		todoBox.insertAdjacentHTML('afterbegin',newTodo);
+	}
 
-		function LOA(el)
-		{	
-			let status = el.nextElementSibling.classList.toggle('fa-check');
-			syncLocalStorage('UPDATE', el.innerText, status);
-		}
+	function checked(el)
+	{	
+		let status = el.nextElementSibling.classList.toggle('fa-check');
+		syncLocalStorage('CHECK', el.innerText, status);
+	}
+
+	function hapus(el){
+		if(confirm('Press OKE if you wanna delete this or press CANCEl if you wanna check this'))
+		{
+			el.remove();
+			syncLocalStorage('DELETE', el.innerText.trim());
+		};
+	}
